31ea02a52789c49a51e30f7e8fcb7a849b3a491b
yungo1846 <yungo1468@naver.com>
2025-01-18 18:31:20 +0900

    chore: es-toolkit

--- src/components/mdx/list/index.tsx
+++ src/components/mdx/list/index.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import { Children, ComponentProps } from 'react';
 import { Paragraph } from '../paragraph';
 import { listCss } from './index.css';
+import { isObject } from 'es-toolkit/compat';
 
 type Props = ComponentProps<'li'>;
 
@@ -19,7 +20,12 @@ export const List = ({ className, ...props }: Props) => {
       );
     }
 
-    if (child.type.name === 'Paragraph') {
+    if (
+      isObject(child) &&
+      'type' in child &&
+      isObject(child.type) &&
+      child.type.name === 'Paragraph'
+    ) {
       return <li className={clsx(listCss, className)}>{child}</li>;
     }
 
